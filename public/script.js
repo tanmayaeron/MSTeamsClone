949465c33cb18f4a0070f099fd8661b629835cac
Tanmay Aeron <2019csb1124@iitrpr.ac.in>
2021-06-27 21:49:03 +0530

    resolved bug
    In users who joined later than user who left, video was not removed.

--- public/script.js
+++ public/script.js
@@ -16,6 +16,9 @@ navigator.mediaDevices.getUserMedia({
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
+    call.on('close', () => {
+      video.remove()
+    })
   })
 
   socket.on('user-connected', userId => {
